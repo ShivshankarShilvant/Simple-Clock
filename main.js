666e5c0bdfaad4875366194d55bbdccbe8b8520c
ShivshankarShilvant <shivashilvant190@gmail.com>
2025-11-01 22:30:06 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", ()=>{
+    const tickSound = new Audio("./tick.mp3");
+    tickSound.loop = true;
+    tickSound.play().catch((err)=>{
+        document.addEventListener("click", ()=>{
+            tickSound.play();
+
+        }, {once: true})
+    })
+})
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime
+    srotation = 6 * stime
+
+    hour.style.transform = `rotate(${hrotation}deg)`
+    minute.style.transform = `rotate(${mrotation}deg)`
+    second.style.transform = `rotate(${srotation}deg)`
+
+}, 1000)
